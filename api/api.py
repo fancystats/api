5a6eb5ed05eca53f124170ecf3a1d410665dadc6
Michael Lavers <kolanos@gmail.com>
2015-03-21 15:01:15 -0700

    adding rest resources, api might be an unfortunate name for this module

--- api/api.py
+++ api/api.py
@@ -1,4 +1,89 @@
-from flask import Flask
+from flask_peewee.rest import RestAPI, RestResource
 
-app = Flask(__name__)
-app.config.from_object('api.config.Config')
+from nhlstats import models
+
+from api.app import app
+
+api = RestAPI(app)
+
+
+class ReadOnlyResource(RestResource):
+    """Inherit from this resource if it is read only."""
+
+    def __init__(self, rest_api, model, authentication):
+        allowed_methods=   ['GET']
+        super(ReadOnlyResource, self).__init__(self, rest_api, model,
+                                               authentication,
+                                               allowed_methods=allowed_methods)
+
+
+class ArenaResource(ReadOnlyResource):
+    pass
+
+
+class LeagueResource(ReadOnlyResource):
+    pass
+
+
+class SeasonResource(ReadOnlyResource):
+    pass
+
+
+class ConferenceResource(ReadOnlyResource):
+    pass
+
+
+class DivisionResource(ReadOnlyResource):
+    pass
+
+
+class TeamResource(ReadOnlyResource):
+    pass
+
+
+class ScheduleResource(ReadOnlyResource):
+    pass
+
+
+class PlayerResource(ReadOnlyResource):
+    pass
+
+
+class PlayerSkaterStatResource(ReadOnlyResource):
+    pass
+
+
+class PlayerGoalieStatResource(ReadOnlyResource):
+    pass
+
+
+class CoachResource(ReadOnlyResource):
+    pass
+
+
+class GameResource(ReadOnlyResource):
+    pass
+
+
+class EventResource(ReadOnlyResource):
+    pass
+
+
+class PlayerEventResource(ReadOnlyResource):
+    pass
+
+
+api.register(models.Arena, ArenaResource)
+api.register(models.League, LeagueResource)
+api.register(models.Season, SeasonResource)
+api.register(models.Conference, ConferenceResource)
+api.register(models.Division, DivisionResource)
+api.register(models.Team, TeamResource)
+api.register(models.Schedule, ScheduleResource)
+api.register(models.Player, PlayerResource)
+api.register(models.PlayerSkaterStat, PlayerSkaterStatResource)
+api.register(models.PlayerGoalieStat, PlayerGoalieStatResource)
+api.register(models.Coach, CoachResource)
+api.register(models.Game, GameResource)
+api.register(models.Event, EventResource)
+api.register(models.PlayerEvent, PlayerEventResource)
